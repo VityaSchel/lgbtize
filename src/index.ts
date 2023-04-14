33b2b13d37573f75eab1ec63b4fa03b886a89752
Viktor <59040542+VityaSchel@users.noreply.github.com>
2023-04-14 04:11:29 +0400

    Telegram bot

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import './env.js'
+import TelegramBot from 'node-telegram-bot-api'
+import tmp from 'temp-dir'
+import { v5 as uuid } from 'uuid'
+import { lgbt } from 'lgbt'
+
+const token = process.env.TOKEN
+if(!token) throw new Error('TOKEN is not defined')
+
+const bot = new TelegramBot(token, { polling: true })
+bot.on('message', async (event) => {
+  if(!event.photo) {
+    await bot.sendMessage(event.chat.id, 'Пришли мне фото')
+    return
+  }
+
+  const photo = event.photo.sort((photo1, photo2) => photo2.width*photo2.height - photo1.width*photo1.height)[0]
+  const file = await bot.downloadFile(photo.file_id, tmp)
+
+  const msg = await bot.sendMessage(event.chat.id, 'Пидоризация... 🏳️‍🌈')
+  
+  const result = await lgbt(file)
+  await bot.deleteMessage(event.chat.id, msg.message_id)
+  await bot.sendPhoto(event.chat.id, result)
+})
